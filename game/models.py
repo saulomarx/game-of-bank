cf5ff466f00867312bc3c267f84657275203480d
saulomarx <saulo.marx@hotmail.com>
2021-02-15 21:15:40 -0300

    Add players behaviour

--- game/models.py
+++ game/models.py
@@ -1,11 +1,59 @@
+from random import getrandbits
+
+
 class Player:
     def __init__(self, wallet=300):
         self.wallet = wallet
-        self.title = 0
+        self.position = 0
+
+
+class ImpulsivePlayer(Player):
+    def __init__(self):
+        super.__init__()
+
+    def will_buy(self, value):
+        if value <= self.wallet:
+            self.wallet = self.wallet - value
+            return True
+        return False
+
+
+class DemandingPlayer(Player):
+    def __init__(self):
+        super.__init__()
+
+    def will_buy(self, value):
+        if self.wallet >= value > 50:
+            self.wallet = self.wallet - value
+            return True
+        return False
+
+
+class CautiousPlayer(Player):
+    def __init__(self):
+        super.__init__()
+
+    def will_buy(self, value):
+        is_buying = (self.wallet - value) >= 80
+        if is_buying:
+            self.wallet = self.wallet - value
+            return True
+        return False
+
+
+class RandomPlayer(Player):
+    def __init__(self):
+        super.__init__()
+
+    def will_buy(self, value):
+        is_buying = bool(getrandbits(1))
+        if is_buying and value <= self.wallet:
+            self.wallet = self.wallet - value
+            return True
+        return False
 
 
 class Building:
     def __init__(self, value):
         self.value = value
         self.owner = None
-
