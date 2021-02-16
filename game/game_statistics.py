93c165efafd46b6c4a5bcd1f0c8658976af14f5e
saulomarx <saulo.marx@hotmail.com>
2021-02-16 19:26:12 -0300

    - Add avg round metrics

--- game/game_statistics.py
+++ game/game_statistics.py
@@ -1,6 +1,6 @@
 # Quantas partidas terminam por time out (1000 rodadas); Done
 #
-# Quantos turnos em média demora uma partida;
+# Quantos turnos em média demora uma partida; done
 #
 # Qual a porcentagem de vitórias por comportamento dos jogadores;
 #
@@ -13,6 +13,15 @@ def _count_timeout(game_results, max_rounds):
     return timeout_count
 
 
+def _avg_rounds(game_results):
+    rounds_sum = 0
+    for result in game_results:
+        rounds_sum += result.number_of_round
+    return rounds_sum / len(game_results)
+
+
 def show_game_data(game_results, max_rounds):
     timeout_count = _count_timeout(game_results, max_rounds)
-    print(timeout_count)
+    avg_rounds = _avg_rounds(game_results)
+    print("Numero de jogos com timeout:", timeout_count)
+    print("Duracao media de rounds:", avg_rounds)
